bebe16d28abbe473d833ce469dd2e62d6f75fbaf
Gergo N <csovis@gmail.com>
2022-10-22 14:55:55 +0200

    setup webpack and npm project

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var config = function (devMode) {
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [
+                        devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ],
+                }
+            ],
+        },
+        plugins: [new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/assets/index.html'
+        })].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    }
+};
+
+module.exports = (env, argv) => {
+    return config(argv.mode !== 'production');
+};

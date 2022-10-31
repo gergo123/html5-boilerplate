97dc04d309d9d68b07f0ad82a80211ac1d766172
Gergo N <csovis@gmail.com>
2022-10-31 11:36:31 +0100

    rename hello world component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import HelloWorld from "./HelloWorld";
+import HelloWorld from "./hello-world";
 
 import './styles.css'
 
